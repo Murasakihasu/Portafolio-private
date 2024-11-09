ba60acc12addbae631f8d5e2ca55a1bd16c69e00
Martin Victoriano <martinalegonvicto@gmail.com>
2024-11-09 13:28:11 -0300

    probando home 2

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-// Seleccionamos todos los elementos con la clase 'iconBox'
 const iconBoxes = document.querySelectorAll('.iconBox');
 
 iconBoxes.forEach(iconBox => {
   iconBox.addEventListener('click', () => {
-    // Quitamos la clase 'active' de cualquier tarjeta que la tenga
     document.querySelectorAll('.tarjeta.active').forEach(activeTarjeta => {
       activeTarjeta.classList.remove('active');
     });
-    
-    // Añadimos la clase 'active' a la tarjeta que contiene el iconBox clicado
     const tarjeta = iconBox.closest('.tarjeta');
     tarjeta.classList.add('active');
   });
@@ -17,32 +13,28 @@ iconBoxes.forEach(iconBox => {
 
 const toggleButton = document.getElementById("toggle");
 const navbar = document.getElementById("navbar");
-const menuItems = navbar.querySelectorAll("a"); // Selecciona las etiquetas dentro del menú (por ejemplo, enlaces)
+const menuItems = navbar.querySelectorAll("a");
 
-// Función para cerrar el menú
 function closeMenu() {
   navbar.classList.remove("active");
 }
 
-// Alterna la clase 'active' en el navbar al hacer clic en el toggle
 toggleButton.addEventListener("click", (event) => {
   navbar.classList.toggle("active");
-  event.stopPropagation(); // Evita que el evento se propague y cierre el menú inmediatamente
+  event.stopPropagation();
 });
 
-// Cierra el menú al hacer clic en cualquier etiqueta dentro del menú
 menuItems.forEach((item) => {
   item.addEventListener("click", closeMenu);
 });
 
-// Cierra el menú al hacer clic fuera del menú
 document.addEventListener("click", (event) => {
   if (!navbar.contains(event.target) && !toggleButton.contains(event.target)) {
     closeMenu();
   }
 });
 
-// Código para manejar el botón "showMore"
+
 const showMoreButtons = document.querySelectorAll(".showMore");
 
 showMoreButtons.forEach((button) => {
@@ -55,47 +47,35 @@ showMoreButtons.forEach((button) => {
 });
 
 
-// Selecciona todos los elementos con la clase "bar80"
 const barElements = document.querySelectorAll('.bar8');
 
-// Configura el observador para cada elemento
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      // Agrega la clase para activar la animación
       entry.target.classList.add('crecer80');
-      // Deja de observar después de activar la animación
       observer.unobserve(entry.target);
     }
   });
 }, {
-  threshold: 0.5 // Ajusta el porcentaje de visibilidad
+  threshold: 0.5 
 });
 
-// Observa cada elemento con la clase "bar80"
 barElements.forEach(bar => observer.observe(bar));
 
 
-
-
-// Selecciona todos los elementos con la clase "bar80"
 const bar2Elements = document.querySelectorAll('.bar2');
 
-// Configura el observador para cada elemento
 const observer2 = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      // Agrega la clase para activar la animación
       entry.target.classList.add('crecer20');
-      // Deja de observar después de activar la animación
       observer2.unobserve(entry.target);
     }
   });
 }, {
-  threshold: 0.5 // Ajusta el porcentaje de visibilidad
+  threshold: 0.5 
 });
 
-// Observa cada elemento con la clase "bar80"
 bar2Elements.forEach(bar => observer2.observe(bar));
 
 
